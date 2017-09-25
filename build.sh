8ed6d3132d422b02b485a2fac53f12de7cc3a3bd
sherman Rose <sherman@shermanrose.uk>
2017-09-25 12:16:29 +0100

    Finsihed setup and installation script of certbot + AWS support

--- build.sh
+++ build.sh
@@ -6,25 +6,25 @@
 # Will setup a server to act as the vTime Let's Encrypt host  
 # For more information consult 'docs/'
 
+
 ## VARIABLES
-CERTBOT_VERSION="v0.18.2"
+CERTBOT_VERSION="0.18.2" #Leave out the v, it will be added later
 WORK_DIR="/tmp/build"
-PRE_REQS="awscli curl python2.7 python-pip"
 
 ## HELPER VARIABLES - Only touch if you know what is going on here
-CERTBOT_DIR="$WORK_DIR/certbot-$CERTBOT_VERSION"
+CERTBOT_DIR="$WORK_DIR/certbot-${CERTBOT_VERSION}"
+APT_PRE_REQS="curl python python-dev libssl-dev libffi-dev build-essential"
 APT_CACHE="true"
 APT_CACHE_URL='"http://172.17.0.1:3142";'
 
+
 ## USEFUL FUNCTIONS
 
-msg(){
-  echo "=====> $@"
-}
-
+# tagged echo
+msg(){ echo "=====> $@"; }
 # Check a command exists silently 
 check() { command -v "$1" 2>/dev/null ; }
-# Easy exit with message
+# Easy exit with message [ERROR] tag will make it easier for monitoring
 bail() { echo "[ERROR] $1"; exit 1; }
 
 # SCRIPT FUNCTIONS
@@ -44,8 +44,12 @@ setup() {
   # Need to ensure we have some prequsq
   msg "Running apt! (this will take some time)"
   export DEBIAN_FRONTEND=noninteractive
-  apt-get -qq update && apt-get install -y $PRE_REQS >/dev/null
-}
+  apt-get -qq update && apt-get install -y $APT_PRE_REQS 
+  msg "Download pip from not apt..."
+  curl https://bootstrap.pypa.io/get-pip.py | python
+  msg "Install Non Debian aws cli"
+  pip install awscli
+}                                   
 
 get_certbot() {
   msg "Getting Certbot Release $CERTBOT_VERSION"
@@ -53,19 +57,36 @@ get_certbot() {
   curl --output "$CERTBOT_DIR.tar.gz" \
        --location \
        --silent \
-       "https://github.com/certbot/certbot/archive/$CERTBOT_VERSION.tar.gz"
+       "https://github.com/certbot/certbot/archive/v$CERTBOT_VERSION.tar.gz"
   tar -xzf "$CERTBOT_DIR.tar.gz" -C "$WORK_DIR"
 }
+
+
+install_certbot(){
+  msg "Installing Certbot" 
+  cd "$CERTBOT_DIR"
+  python setup.py install
+  msg "Installing AWS Route 53 support"
+  cd "$CERTBOT_DIR/certbot-dns-route53"
+  python setup.py install
+} 
+
+# test certbot installation
 
 
 ## START BUIDLING!
 
 # Check out working dir doesn't exit, remove it if it does and then setup for build
 [[ ! -d "$WORK_DIR"  ]] || cleanup 
+
+# Get all of te pre reqs
 setup
 
 # Get the specified version of certbot 
 get_certbot
+
+# Installation of certbot and route53 module
+install_certbot
 
 # Testing pause in case of insepction needs
 while true; do 
